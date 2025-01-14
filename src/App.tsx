5f54a7e717605bc3cc0fe6eaa62c8a30caf0d972
Nguyen Do <nguyendo@Nguyens-MacBook-Pro.local>
2025-01-14 23:50:07 +0700

    feat(package): add tailwindcss package

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
 import WalletPage from "./pages/Wallets";
+import FancyForms from "./pages/FancyForms";
+import './output.css';
+import './index.css';
 
 function App() {
 
   return (
     <>
-      <div>
-          <WalletPage />
-      </div>
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <FancyForms/>
+            <WalletPage/>
+        </div>
     </>
   )
 }
